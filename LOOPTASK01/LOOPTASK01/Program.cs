8dab4bfd948ea9b105e05f3f3dd920974e5a783e
Logan Tawhiri <30024380@student.toiohomai.ac.nz>
2019-03-13 11:39:46 +1300

    cop

---
+++ LOOPTASK01/LOOPTASK01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOOPTASK01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            int limit = 10;
+
+            for (int counter = 1; counter <= limit; counter++)
+            {                
+                Console.Write(counter * 10 + "\t");               
+                Console.Write(counter * 100 + "\t");
+                Console.Write(counter * 1000 + "\t");
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+
+}
